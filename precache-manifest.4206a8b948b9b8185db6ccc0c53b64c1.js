d8894e7f4bfcf8594eb91e8bd00a0fab7ffc837e
Dreamwalker <aristojeff@gmail.com>
2019-04-24 10:23:33 +0900

    Updates

---
+++ precache-manifest.4206a8b948b9b8185db6ccc0c53b64c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8dd0a9e3b388c1b5a5d4c655bb60978",
+    "url": "/react_nasa_app/index.html"
+  },
+  {
+    "revision": "e3435593adda0bcc9beb",
+    "url": "/react_nasa_app/static/css/main.d7d33921.chunk.css"
+  },
+  {
+    "revision": "6de3f36c0b73fe9e36ed",
+    "url": "/react_nasa_app/static/js/2.85510bbc.chunk.js"
+  },
+  {
+    "revision": "e3435593adda0bcc9beb",
+    "url": "/react_nasa_app/static/js/main.caca46d2.chunk.js"
+  },
+  {
+    "revision": "8049ec11c00e202d5087",
+    "url": "/react_nasa_app/static/js/runtime~main.1259c413.js"
+  }
+]);

1f009d96d80dfa418d8d537ce181942ff2227daf
Dreamwalker <aristojeff@gmail.com>
2019-04-24 10:13:12 +0900

    implement api code

--- src/components/ViewerTemplate/ViewerTemplate.js
+++ src/components/ViewerTemplate/ViewerTemplate.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import styles from './ViewerTemplate.scss';
-import classNames from 'classnames/bind';
+import styles from "./ViewerTemplate.scss";
+import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
-const ViewerTemplate = ({viewer, spaceNavigator }) => {
-    return (
-        <div className={cx('viewer-template')}>
-            
-        </div>
-    );
+const ViewerTemplate = ({ viewer, spaceNavigator }) => {
+  return (
+    <div className={cx("viewer-template")}>
+      <header>Astronomy Picture of the Day</header>
+      <div className={cx("viewer-wrapper")}>
+        {viewer}
+        <div className={cx("space-navigator-wrapper")}>{spaceNavigator}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ViewerTemplate;

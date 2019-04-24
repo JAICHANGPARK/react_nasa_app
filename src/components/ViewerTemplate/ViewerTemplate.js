fb7bebe61076d982866cf900f8f35ffead7e4b96
Dreamwalker <aristojeff@gmail.com>
2019-04-24 09:23:19 +0900

    Add Viewer Template

---
+++ src/components/ViewerTemplate/ViewerTemplate.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import styles from './ViewerTemplate.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const ViewerTemplate = ({viewer, spaceNavigator}) => {
+    return (
+        <div className={cx('viewer-template')}>
+            
+        </div>
+    );
+};
+
+export default ViewerTemplate;
